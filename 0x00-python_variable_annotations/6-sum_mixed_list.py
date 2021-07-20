e6795a300e0f9d135fe1e0f05b54a8bc7e9b2c44
EduardoVega04 <eduarvega04@hotmail.com>
2021-07-20 00:04:43 -0500

    Union in list variable annotations

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -2,10 +2,10 @@
 """Type-annotated function sum_mixed_list which takes a
 list mxd_lst of integers and floats and returns their sum as a float"""
 
-from typing import List
+from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: List[int, float]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """Returns the sum of the list as a float"""
     total: float = 0.0
     for i in mxd_lst:
