933b0014baf30ff1552286f7896a8910bd1a9449
EduardoVega04 <eduarvega04@hotmail.com>
2021-08-18 14:24:48 -0500

    Tuple annotation

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -9,4 +9,5 @@ from typing import Union, Tuple
 
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """Returns a tuple. Fiest element is k, second element is square of v"""
     return (k, (v * v))
