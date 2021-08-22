9a4d5762f25c576daf3af17da2affa13e5ec6736
EduardoVega04 <eduarvega04@hotmail.com>
2021-08-22 04:15:36 -0500

    FIFO cache system

--- 0x03-caching/1-fifo_cache.py
+++ 0x03-caching/1-fifo_cache.py
@@ -9,16 +9,16 @@ class FIFOCache(BaseCaching):
     def __init__(self):
         """Initializes the FIFO cache system"""
         super().__init__()
+        self.cache_queue = []
 
     def put(self, key, item):
         """Puts an entry in the FIFO system"""
-        fifo_queue_list = []
         if key and item:
             self.cache_data[key] = item
-            fifo_queue_list.append(key)
+            self.cache_queue.append(key)
 
         if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            removed = fifo_queue_list.pop(0)
+            removed = self.cache_queue.pop(0)
             del self.cache_data[removed]
             print(f"DISCARD: {removed}")
 
